c760a4e79f95551bfe80d1aa8c906987331d16b7
Vadim Salavatov <xremmargorpx@gmail.com>
2019-10-05 11:40:51 +0300

    +AuthenticationResult

---
+++ src/main/kotlin/snailmail/core/api/AuthenticationResult.kt
@@ -0,0 +1,22 @@
+package snailmail.core.api
+
+typealias AuthToken = String
+
+sealed class AuthenticationResult {
+    abstract val successful: Boolean
+}
+
+class AuthSuccessful(val token: AuthToken) : AuthenticationResult() {
+    override val successful: Boolean
+        get() = true
+}
+
+class AuthWrongCredentials : AuthenticationResult() {
+    override val successful: Boolean
+        get() = false
+}
+
+class AuthError : AuthenticationResult() {
+    override val successful: Boolean
+        get() = false
+}
