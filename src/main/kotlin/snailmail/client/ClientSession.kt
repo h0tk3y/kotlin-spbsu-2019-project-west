87940aac9c5046a42a8c88f56eec4d0676c36f2b
Ольга Шиманская <st069066@student.spbu.ru>
2019-10-05 17:23:59 +0300

    fixed

--- src/main/kotlin/snailmail/client/ClientSession.kt
+++ src/main/kotlin/snailmail/client/ClientSession.kt
@@ -14,27 +14,32 @@ class ClientSession(val ClientAPI : API) {
     }
 
     private fun findUser(username : String) : User? {
-        val user = ClientAPI.searchByUsername(username)
-        if (user == null) {
-            println("This user doesn't exist")
-        }
-        return user
+        return ClientAPI.searchByUsername(username)
     }
 
     fun startSession(){
         while(true) {
             print("Welcome to SnailMail! Do you have an account? (y/n)")
+            var isSuccess = false
             val answer = readLine()!!
             var userCredentials = getUserCredentials();
             if (answer == "y") {
-                var isSuccess = false
                 do {
-                    userCredentials = getUserCredentials()
                     isSuccess = ClientAPI.authenticate(userCredentials).successful
+                    if (!isSuccess) {
+                        println("Username or/and password are incorrect")
+                        userCredentials = getUserCredentials()
+                    }
                 } while (!isSuccess)
                 break;
             } else if (answer == "n") {
-                ClientAPI.register(userCredentials).successful
+                do {
+                    isSuccess = ClientAPI.register(userCredentials).successful
+                    if (!isSuccess) {
+                        println("Registration failed, try to change username")
+                        userCredentials = getUserCredentials()
+                    }
+                } while (!isSuccess)
                 break;
             } else {
                 print("Incorrect format of the answer, answer again: ")
@@ -47,19 +52,22 @@ class ClientSession(val ClientAPI : API) {
         var isSuccess = true;
         when(args[0]) {
             "/send" -> {
-                if (args.size == 3) {
+                if (args.size >= 3) {
+                    val message = args.filterIndexed { index, _ -> index >= 2 }
+                            .joinToString(separator = " ")
                     val user = findUser(args[1])
                     if (user == null) {
+                        println("This user doesn't exist")
                         isSuccess = false
                     } else {
-                        ClientAPI.sendTextMessage(args[2],
+                        ClientAPI.sendTextMessage(message,
                                 ClientAPI.getPersonalChatWith(user))
                     }
                 } else isSuccess = false
             }
-            "/viewAwailableChats" -> {
-                val awailableChats = ClientAPI.getAvailableChats().getChats()
-                for (chat in awailableChats) {
+            "/viewAvailableChats" -> {
+                val availableChats = ClientAPI.getAvailableChats().getChats()
+                for (chat in availableChats) {
                     println(chat)
                 }
             }
@@ -70,6 +78,7 @@ class ClientSession(val ClientAPI : API) {
                 if (args.size == 2) {
                     val user = findUser(args[1])
                     if (user == null) {
+                        println("This user doesn't exist")
                         isSuccess = false
                     } else {
                         println(user)
@@ -80,6 +89,7 @@ class ClientSession(val ClientAPI : API) {
                 if (args.size == 2) {
                     val user = findUser(args[1])
                     if (user == null) {
+                        println("This user doesn't exist")
                         isSuccess = false
                     } else {
                         val chat = ClientAPI.getPersonalChatWith(user)
