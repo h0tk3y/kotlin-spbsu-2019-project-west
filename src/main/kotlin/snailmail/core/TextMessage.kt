5b8894ecd990dbd28ae47ac77868d0983a5f0587
Vadim Salavatov <xremmargorpx@gmail.com>
2019-10-05 12:10:25 +0300

    +Message.chatId

--- src/main/kotlin/snailmail/core/TextMessage.kt
+++ src/main/kotlin/snailmail/core/TextMessage.kt
@@ -4,11 +4,12 @@ import java.util.*
 
 class TextMessage(
         id: UUID,
+        chatId: UUID,
         sender: UUID,
         date: Date,
         seen: Boolean = false,
         val content: String
-) : Message(id, sender, date, seen) {
+) : Message(id, chatId, sender, date, seen) {
     override fun toString(): String {
         return "at $date: $content"
     }
--- src/main/kotlin/snailmail/core/UserJoinedMessage.kt
+++ src/main/kotlin/snailmail/core/UserJoinedMessage.kt
@@ -2,6 +2,6 @@ package snailmail.core
 
 import java.util.*
 
-class UserJoinedMessage(id: UUID, date: Date, val user: UUID) : ServiceMessage(id, date) {
+class UserJoinedMessage(id: UUID, chatId: UUID, date: Date, val user: UUID) : ServiceMessage(id, chatId, date) {
     override fun getText(): String = "User with ID $user joined this chat"
 }
