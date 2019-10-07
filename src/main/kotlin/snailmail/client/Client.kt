19354b300ad16d2e4eadfc07edff5bf50672b2c9
Ольга Шиманская <st069066@student.spbu.ru>
2019-10-07 22:16:30 +0300

    added fixes

--- src/main/kotlin/snailmail/client/Client.kt
+++ src/main/kotlin/snailmail/client/Client.kt
@@ -8,41 +8,36 @@ import snailmail.server.Server
 class Client(val server : Server) {
     private var token : AuthToken? = null
 
-    private fun checkToken() : AuthToken {
-        val t = token
-        if (t == null)
-            throw NullTokenException("Token doesn't exist")
-        else
-            return t
-    }
+    private fun acquireToken() : AuthToken =
+        token ?: throw NoAuthTokenException("Token doesn't exist")
 
     fun findUser(username : String) : User {
-        val t = checkToken()
+        val t = acquireToken()
         return server.searchByUsername(t, username) ?:
         throw UserNotFoundException("This user doesn't exist")
     }
 
     fun sendMessage(username: String, message: String) : TextMessage {
         val user = findUser(username)
-        val t = checkToken()
+        val t = acquireToken()
         return server.sendTextMessage(t, message,
                 server.getPersonalChatWith(t, user.id).id)
     }
 
     fun findAvailableChats() : List<Chat> {
-        val t = checkToken()
+        val t = acquireToken()
         return server.getAvailableChats(t).getChats()
     }
 
     private fun findPersonalChat(username: String) : Chat {
         val user = findUser(username)
-        val t = checkToken()
+        val t = acquireToken()
         return server.getPersonalChatWith(t, user.id)
     }
 
     fun getPersonalChatHistory(username: String) : List<Message> {
         val chat = findPersonalChat(username)
-        val t = checkToken()
+        val t = acquireToken()
         return server.getChatMessages(t, chat.id).getMessages()
     }
 
