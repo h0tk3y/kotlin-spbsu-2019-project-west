9de69d534aad98762b3e9dc039e8ae53c6c4a4d7
Vadim Salavatov <xremmargorpx@gmail.com>
2019-10-11 23:08:12 +0300

    Some basis for server-protocol-protocol-client implementation

---
+++ src/main/kotlin/snailmail/standalone/client/StandaloneClient.kt
@@ -0,0 +1,5 @@
+package snailmail.standalone.client
+
+fun main() {
+    println("Hello, it is client!")
+}
---
+++ src/main/kotlin/snailmail/standalone/server/StandaloneServer.kt
@@ -0,0 +1,5 @@
+package snailmail.standalone.server
+
+fun main() {
+    println("Hello, it is server!")
+}
