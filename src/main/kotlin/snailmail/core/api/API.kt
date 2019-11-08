67080a0e7d60c79d03dbe1df56ff1d72b2df033b
Nikita Tsarev <nikts53@gmail.com>
2019-11-08 17:03:58 +0300

    Unified API interfaces

--- src/main/kotlin/snailmail/core/api/API.kt
+++ src/main/kotlin/snailmail/core/api/API.kt
@@ -1,3 +1,16 @@
 package snailmail.core.api
 
-interface API : AuthAPI, ChatAPI, MessageAPI, UserAPI
+import snailmail.core.*
+import java.util.*
+
+interface API {
+    fun authenticate(credentials: UserCredentials): AuthToken
+    fun register(credentials: UserCredentials): AuthToken
+    fun getAvailableChats(token: AuthToken): List<Chat>
+    fun getPersonalChatWith(token: AuthToken, user: UUID): PersonalChat
+    fun createGroupChat(token: AuthToken, title: String, invitedMembers: List<UUID>): GroupChat
+    fun getChatMessages(token: AuthToken, chat: UUID): List<Message>
+    fun sendTextMessage(token: AuthToken, text: String, chat: UUID): TextMessage
+    fun searchByUsername(token: AuthToken, username: String): User?
+    fun getUserById(token: AuthToken, id: UUID): User?
+}
--- src/main/kotlin/snailmail/core/api/AuthAPI.kt
+++
@@ -1,8 +0,0 @@
-package snailmail.core.api
-
-import snailmail.core.UserCredentials
-
-interface AuthAPI {
-    fun authenticate(credentials: UserCredentials): AuthToken
-    fun register(credentials: UserCredentials): AuthToken
-}
